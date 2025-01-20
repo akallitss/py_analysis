6779c3c6622e31fb0a964aead3495ee11ac3d993
akallits <alexandra.kallitsopoulou@cern.ch>
2025-01-21 00:29:44 +0100

    main() example

---
+++ scripts/main_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -- coding: utf-8 --
+"""
+Created on January 20 17:53 2025
+Created in PyCharm
+Created as py_analysis/main_test.py
+
+@author: akallitss, akallits
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    print(f'main_test namespace: {__name__}')
+    print('bonzo')
+
+
+if __name__ == '__main__':
+    main()
