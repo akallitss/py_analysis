50e2cfde6c4e67646b99b0b5e98bc817f271717f
Dylan Neff <Dyn0402@hotmail.com>
2025-03-08 19:04:22 +0100

    pulse_finding_alg.py added find_pulse_bounds2 with 3 step pulse finding

--- TriggerEvaluation/pulse_finding_alg.py
+++ TriggerEvaluation/pulse_finding_alg.py
@@ -96,6 +96,7 @@ def main():
 
     x_int, y_int = integral_numpy(x_time, total_waveform, window_finding_integration_points)
     x_bounds = find_pulse_bounds(x_int, y_int, n_pt_threshold, electron_peak_width, ion_tail_width)
+    x_bounds = find_pulse_bounds2(x_time, total_waveform, n_pt_threshold, electron_peak_width, ion_tail_width, window_finding_integration_points)
 
     fig_wave, ax_wave = plt.subplots(figsize=(8, 6))
     ax_wave.plot(x_time, total_waveform, color='blue')
@@ -147,6 +148,27 @@ def main():
     ax_int_sec.grid(False)
     fig_int_sec.tight_layout()
 
+    x_avg_sec, y_avg_sec = moving_average(x_time, total_waveform, secondary_finding_integration_points)
+    x_avg_deriv_sec, y_avg_deriv_sec = derivative_numpy(x_avg_sec, y_avg_sec)
+    x_avg_deriv_avg_sec, y_avg_deriv_avg_sec = moving_average(x_avg_deriv_sec, y_avg_deriv_sec, secondary_finding_integration_points)
+    # Plot
+    fig_avg_sec, ax_avg_sec = plt.subplots(figsize=(8, 6))
+    ax_avg_sec.plot(x_avg_sec, y_avg_sec, color='blue')
+    ax_avg_sec.plot(x_avg_deriv_sec, y_avg_deriv_sec, color='red')
+    ax_avg_sec.plot(x_avg_deriv_avg_sec, y_avg_deriv_avg_sec, color='orange')
+    # ax_avg_sec.axhline(n_pt_threshold, color='red', linestyle='--')
+    for (x_left, x_right) in x_bounds:
+        ax_avg_sec.axvline(x_left, color='green', linestyle='-')
+        ax_avg_sec.axvline(x_right, color='green', linestyle='-')
+    ax_avg_sec.set_xlabel('Time (ns)', fontsize=16, fontweight='bold', family='serif')
+    ax_avg_sec.set_ylabel('Voltage (V)', fontsize=16, fontweight='bold', family='serif')
+    ax_avg_sec.set_title('Waveform Moving Average', fontsize=18, fontweight='bold', family='serif')
+    ax_avg_sec.tick_params(axis='both', which='both', direction='in', length=8, width=2, labelsize=14)
+    for spine in ax_avg_sec.spines.values():
+        spine.set_linewidth(2)
+    ax_avg_sec.grid(False)
+    fig_avg_sec.tight_layout()
+
     y_cdf = get_cdf(x_int, y_int)
     fig_cdf, ax_cdf = plt.subplots(figsize=(8, 6))
     ax_cdf.plot(x_int, y_cdf, color='blue')
@@ -196,7 +218,10 @@ def find_pulse_bounds(x_int, y_int, threshold, electron_peak_width=5, ion_tail_w
         # Get min point within ion tail width
         x_trigger = x_int[i_trigger]
         x_end = x_trigger + ion_tail_width
-        i_end = np.where(x_int > x_end)[0][0]
+        try:
+            i_end = np.where(x_int > x_end)[0][0]
+        except IndexError:  # Too close to the end of the waveform
+            break
 
         i_min = np.argmin(y_int[i_trigger:i_end]) + i_trigger
         y_min = y_int[i_min]
@@ -206,16 +231,179 @@ def find_pulse_bounds(x_int, y_int, threshold, electron_peak_width=5, ion_tail_w
         print(x_trigger, x_end, y_min, y_end)
 
         # Get first point to left of minimum above end fraction of min
-        i_left = np.where(y_int[i_start:i_min] > y_end)[0][-1] + i_start
+        try:
+            i_left = np.where(y_int[i_start:i_min] > y_end)[0][-1] + i_start
+        except IndexError:
+            i_left = i_start
 
         # Get first point to right of minimum above end fraction of min
-        i_right = np.where(y_int[i_min:] > y_end)[0][0] + i_min
+        try:
+            i_right = np.where(y_int[i_min:] > y_end)[0][0] + i_min
+        except IndexError:
+            i_right = len(x_int) - 1
 
         signal_bounds.append((x_int[i_left], x_int[i_right]))
         print(signal_bounds)
         i_start = i_right
 
     return signal_bounds
+
+
+def find_pulse_bounds2(x_time, y_waveform, threshold, electron_peak_width=5, ion_tail_width=100, trigger_points=50, dt=0.1):
+    """
+    Find the bounds of a pulse in a waveform. Get the min point and then find the points where the integral
+    drops above a fraction of this min on either side of the min point.
+    Args:
+        x_time:
+        y_waveform:
+        threshold:
+        electron_peak_width:
+        ion_tail_width:
+        end_frac:
+
+    Returns:
+
+    """
+    threshold /= trigger_points
+    end_thresh = threshold * 0.5
+
+    # Find all triggers and rough bounds with trigger points
+    x_trigger_mv_avg, y_trigger_mv_avg = moving_average(x_time, y_waveform, trigger_points)
+
+    signal_bounds = []
+    i_start = 0
+    while True:
+        # Get first point below threshold
+        i_triggers = np.where(y_trigger_mv_avg[i_start:] < threshold)[0]
+        if len(i_triggers) == 0:  # If no triggers found, waveform is finished
+            break
+        i_trigger = i_triggers[0] + i_start
+
+        # Get min point within ion tail width
+        x_trigger = x_trigger_mv_avg[i_trigger]
+        x_end = x_trigger + ion_tail_width
+        if x_end > x_time[-1]:
+            break  # Too close to end of waveform
+        i_end = np.where(x_trigger_mv_avg > x_end)[0][0]
+
+        print(f'i_trigger: {i_trigger}, i_end: {i_end}, x_trigger: {x_trigger}, x_end: {x_end}')
+        i_min = np.argmin(y_trigger_mv_avg[i_trigger:i_end]) + i_trigger
+
+        # Get first point to left of minimum above end fraction of min
+        i_left = np.where(y_trigger_mv_avg[i_start:i_min] > end_thresh)[0][-1] + i_start
+
+        # Get first point to right of minimum above end fraction of min
+        i_right = np.where(y_trigger_mv_avg[i_min:] > end_thresh)[0][0] + i_min
+
+        signal_bounds.append((x_trigger_mv_avg[i_left], x_trigger_mv_avg[i_right]))
+        i_start = i_right
+
+    fig, ax = plt.subplots()
+    ax.plot(x_time, y_waveform, color='blue')
+    ax.plot(x_trigger_mv_avg, y_trigger_mv_avg, color='red')
+    ax.axhline(threshold, color='green', linestyle='--')
+    for (x_left, x_right) in signal_bounds:
+        ax.axvline(x_left, color='green', linestyle='-')
+        ax.axvline(x_right, color='green', linestyle='-')
+    ax.set_xlabel('Time (ns)', fontsize=16, fontweight='bold', family='serif')
+    ax.set_ylabel('Voltage (V)', fontsize=16, fontweight='bold', family='serif')
+    ax.set_title('Waveform', fontsize=18, fontweight='bold', family='serif')
+    ax.tick_params(axis='both', which='both', direction='in', length=8, width=2, labelsize=14)
+    for spine in ax.spines.values():
+        spine.set_linewidth(2)
+    ax.grid(False)
+    fig.tight_layout()
+
+    # Check bounds for secondary pulses
+    epeak_npts = int(electron_peak_width / dt)
+    x_epeak_mv_avg, y_epeak_mv_avg = moving_average(x_time, y_waveform, epeak_npts)
+    x_epeak_mv_avg_deriv, y_epeak_mv_avg_deriv = derivative_numpy(x_epeak_mv_avg, y_epeak_mv_avg)
+    x_epeak_mv_avg_deriv_mv_avg, y_epeak_mv_avg_deriv_mv_avg = moving_average(x_epeak_mv_avg_deriv, y_epeak_mv_avg_deriv, epeak_npts)
+
+    # Plot
+    fig, ax = plt.subplots()
+    ax.plot(x_time, y_waveform, color='blue')
+    ax.plot(x_epeak_mv_avg_deriv, y_epeak_mv_avg_deriv, color='red')
+    ax.plot(x_epeak_mv_avg_deriv_mv_avg, y_epeak_mv_avg_deriv_mv_avg, color='orange')
+
+    secondary_reject = [False] * len(signal_bounds)
+    secondary_dydx_threshold = 0.05
+    for bound_i, (x_left, x_right) in enumerate(signal_bounds):
+        # Find min derivative in bounds
+        i_left = np.where(x_epeak_mv_avg_deriv_mv_avg > x_left)[0][0]
+        i_right = np.where(x_epeak_mv_avg_deriv_mv_avg > x_right)[0][0]
+        i_min = np.argmin(y_epeak_mv_avg_deriv_mv_avg[i_left:i_right]) + i_left
+        y_min = y_epeak_mv_avg_deriv_mv_avg[i_min]
+        y_thresh = y_min * secondary_dydx_threshold
+
+        # Look left and right of min for points below threshold. Move 1 electron peak width to each side
+        y_left = y_epeak_mv_avg_deriv_mv_avg[i_left:i_min - int(electron_peak_width / dt)]
+        y_right = y_epeak_mv_avg_deriv_mv_avg[i_min + int(electron_peak_width / dt):i_right]
+        if len(np.where(y_left < y_thresh)[0]) > 0:
+            secondary_reject[bound_i] = True
+        if len(np.where(y_right < y_thresh)[0]) > 0:
+            secondary_reject[bound_i] = True
+
+        ax.axvline(x_left, color='green', linestyle='-')
+        ax.axvline(x_right, color='green', linestyle='-')
+        ax.axhline(y_thresh, color='green', linestyle='--')
+
+    ax.set_xlabel('Time (ns)', fontsize=16, fontweight='bold', family='serif')
+    ax.set_ylabel('Voltage (V)', fontsize=16, fontweight='bold', family='serif')
+    ax.set_title('Waveform', fontsize=18, fontweight='bold', family='serif')
+    ax.tick_params(axis='both', which='both', direction='in', length=8, width=2, labelsize=14)
+    for spine in ax.spines.values():
+        spine.set_linewidth(2)
+    ax.grid(False)
+    fig.tight_layout()
+
+    print(f'Signal Bounds: {signal_bounds}')
+    print(f'Secondary Reject: {secondary_reject}')
+
+    # Use CDF to rework right bound for each window
+    y_cdf = get_cdf(x_time, y_waveform)
+
+    # Plot
+    fig, ax = plt.subplots()
+    ax.plot(x_time, y_waveform, color='blue')
+    ax.plot(x_time, y_cdf, color='red')
+
+    for bound_i, (x_left, x_right) in enumerate(signal_bounds):
+        # Plot original bounds
+        ax.axvline(x_left, color='green', linestyle='-')
+        ax.axvline(x_right, color='green', alpha=0.5, linestyle=':')
+
+        # Expand current window to push right bound to end of waveform or start of next pulse
+        if bound_i == len(signal_bounds) - 1:
+            x_right_push = x_time[-1]
+        else:
+            x_right_push = signal_bounds[bound_i + 1][0]
+
+        print(f'Old Right: {x_right}, New Right: {x_right_push}')
+        print(f'Left: {x_left}, Right: {x_right}')
+
+        # Get maximum of CDF in each window, set this as right bound
+        i_left = np.where(x_time > x_left)[0][0]
+        i_right = np.where(x_time > x_right_push)[0]
+        i_right = i_right[0] if len(i_right) > 0 else len(x_time) - 1
+        i_cdf_max = np.argmax(y_cdf[i_left:i_right]) + i_left
+        x_right_new = x_time[i_cdf_max]
+
+        # Plot new bound
+        ax.axvline(x_right_new, color='green', linestyle='-')
+
+        print(f'Old Right: {x_right}, New Right: {x_right_new}')
+        signal_bounds[bound_i] = (x_left, x_right_new)
+
+    ax.set_xlabel('Time (ns)', fontsize=16, fontweight='bold', family='serif')
+    ax.set_ylabel('Voltage (V)', fontsize=16, fontweight='bold', family='serif')
+    ax.set_title('Waveform', fontsize=18, fontweight='bold', family='serif')
+    ax.tick_params(axis='both', which='both', direction='in', length=8, width=2, labelsize=14)
+    for spine in ax.spines.values():
+        spine.set_linewidth(2)
+    ax.grid(False)
+    fig.tight_layout()
+    plt.show()
 
 
 def identify_secondary_pulses(x_int, y_int, x_left, x_right):
